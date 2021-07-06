059dd63384d5cef4f73005bb321a9375e3fdd862
Hazem <hazem@dgg3d.com>
2021-07-06 20:34:45 +0200

    Minor changes to docker build

--- laravel/docker/build.sh
+++ laravel/docker/build.sh
@@ -12,14 +12,7 @@ composer install --prefer-dist --no-dev --no-interaction --optimize-autoloader
 
 
 # build docker image
-docker build \
-     --build-arg=ENVIRON=$ENVIRON \
-     --build-arg=DB_CONNECTION=$DB_CONNECTION \
-     --build-arg=DB_DATABASE=$DB_DATABASE \
-     --build-arg=DB_USERNAME=$DB_USERNAME \
-     --build-arg=DB_PORT=$DB_PORT \
-     --build-arg=DB_PASSWORD=$DB_PASSWORD \
-      -t $IMAGE:$CI_COMMIT_SHA -f docker/Dockerfile .
+docker build --build-arg=ENVIRON=$ENVIRON -t $IMAGE:$CI_COMMIT_SHA -f docker/Dockerfile .
 
 # push to registry
 if [ "$ENVIRON" != "local" ]; then
